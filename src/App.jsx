e5105dbbab6f89431cbe65ca3979a1aef1572834
Dika Wicaksono <dhkwck00@gmail.com>
2024-05-07 20:37:47 +0700

    changing style again

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,47 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [names, setNames] = useState([
+    "Dika Wicaksono 21.11.4238",
+    "Martinus Juan Prasetyo 21.11.4251",
+    "Rakanannda Erdy S 21.11.4259",
+  ]);
+  const [rainbowIndex, setRainbowIndex] = useState(0);
+  const rainbowColors = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violet",
+  ];
+
+  const handleClick = () => {
+    setNames(shuffleArray([...names]));
+    setRainbowIndex((rainbowIndex + 1) % rainbowColors.length);
+  };
+
+  // Fungsi untuk mengacak array
+  const shuffleArray = (array) => {
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // Selama masih ada elemen yang belum diacak
+    while (0 !== currentIndex) {
+      // Pilih elemen yang tersisa
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // Tukar dengan elemen saat ini
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
 
   return (
     <>
@@ -18,13 +58,20 @@ function App() {
       </div>
       <h1 className="rainbow-text">APLIKASI MATHGROWTH</h1>
       <h3>By</h3>
-      <h2>Dika Wicaksono 21.11.4238</h2>
-      <h2>Martinus Juan Prasetyo 21.11.4251</h2>
-      <h2>Rakanannda Erdy S 21.11.4259</h2>
+      {names.map((name, index) => (
+        <h2
+          key={index}
+          className="name"
+          style={{
+            "--color":
+              rainbowColors[(index + rainbowIndex) % rainbowColors.length],
+          }}
+        >
+          {name}
+        </h2>
+      ))}
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={handleClick}>Acak Nama</button>
       </div>
     </>
   );
